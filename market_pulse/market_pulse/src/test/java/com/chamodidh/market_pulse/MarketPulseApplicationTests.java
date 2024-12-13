ed2f080e427739b340784903b35f6f7e00aa7a6f
ChamodiDH <chamodi3797@gmail.com>
2024-12-13 13:58:10 +0530

    Initialize the project with dependencies

---
+++ market_pulse/market_pulse/src/test/java/com/chamodidh/market_pulse/MarketPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamodidh.market_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
