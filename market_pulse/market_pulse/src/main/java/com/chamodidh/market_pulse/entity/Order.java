36118db0a89b8eb8ff4f4f151b0b86f2321b6ee3
ChamodiDH <chamodi3797@gmail.com>
2024-12-14 23:22:05 +0530

    Implement order entity.

---
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/entity/Order.java
@@ -0,0 +1,43 @@
+package com.chamodidh.market_pulse.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="Order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private Long id;
+
+
+    @Column(name = "ORDER_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Column(name = "TOTAL_AMOUNT")
+    private float totalAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "CUSTOMER_ID", nullable = false)
+    private CustomerDetails customerDetails;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Shipment shipment;
+
+
+
+    //one to one payment
+
+
+    //one to one shipment
+
+
+}
