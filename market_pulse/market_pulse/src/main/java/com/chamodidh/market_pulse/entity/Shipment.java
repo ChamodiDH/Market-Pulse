d41e9569f5d774eb4c58f03be66ba643d911d8c5
ChamodiDH <chamodi3797@gmail.com>
2024-12-15 15:58:28 +0530

    Update Shipment entity.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/entity/Shipment.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/entity/Shipment.java
@@ -21,10 +21,8 @@ public class Shipment {
     @Temporal(TemporalType.DATE)
     private Date estimatedDate;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="TRACK_NO")
-    private Long track_no;
+    @Column(name = "TRACK_NO", unique = true, nullable = false, length = 20)
+    private String trackNo;
 
     @OneToOne
     @JoinColumn(name = "ORDER_ID", nullable = false)
@@ -33,4 +31,16 @@ public class Shipment {
     @ManyToOne
     @JoinColumn(name = "CUSTOMER_ID", nullable = false)
     private CustomerDetails customerDetails;
+
+    @PrePersist
+    private void generateTrackNo() {
+       
+        if (order != null && customerDetails != null) {
+            String orderIdPart = String.format("%05d", order.getId()); // Order ID padded to 5 digits
+            String customerIdPart = String.format("%03d", customerDetails.getId()); // Customer ID padded to 3 digits
+            this.trackNo = orderIdPart + "-" + customerIdPart;
+        } else {
+            throw new IllegalStateException("Order and CustomerDetails must be set before saving Shipment");
+        }
+    }
 }
