4cbd7c0859a23968de724f5ddd3b0cf3cb9776ab
ChamodiDH <chamodi3797@gmail.com>
2024-12-15 21:56:25 +0530

    Implement Usermodel

---
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/model/UserModel.java
@@ -0,0 +1,25 @@
+package com.chamodidh.market_pulse.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserModel {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<String> roles;
+    private List<String> contact;
+    private Date registeredDate;
+}
