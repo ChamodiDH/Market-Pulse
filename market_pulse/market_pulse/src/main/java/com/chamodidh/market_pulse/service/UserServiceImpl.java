2c1b71aae5e52f33afcab195d6e7f28c7e18bcea
ChamodiDH <chamodi3797@gmail.com>
2024-12-16 19:14:12 +0530

    Update user register service.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/UserServiceImpl.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/UserServiceImpl.java
@@ -1,18 +1,25 @@
 package com.chamodidh.market_pulse.service;
 
+import com.chamodidh.market_pulse.entity.CustomerDetails;
+import com.chamodidh.market_pulse.entity.SupplierDetails;
 import com.chamodidh.market_pulse.entity.User;
 import com.chamodidh.market_pulse.model.UserModel;
+import com.chamodidh.market_pulse.repository.CustomerDetailsRepository;
+import com.chamodidh.market_pulse.repository.SupplierDetailsRepository;
 import com.chamodidh.market_pulse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.Date;
 
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    SupplierDetailsRepository supplierDetailsRepository;
+    @Autowired
+    CustomerDetailsRepository customerDetailsRepository;
 
     @Override
     public UserModel userRegister(UserModel userModel) {
@@ -27,6 +34,19 @@ public class UserServiceImpl implements UserService{
        user.setRegisteredDate(new Date());
 
      User savedUser =  userRepository.save(user);
+
+     if(user.getRoles().contains("SUPPLIER")){
+         SupplierDetails supplier = new SupplierDetails();
+         supplier.setUser(user);
+         supplier.setCompanyAddress(userModel.getCompanyAddress());
+         supplierDetailsRepository.save(supplier);
+     }
+     if(user.getRoles().contains("CUSTOMER")){
+            CustomerDetails customer = new CustomerDetails();
+            customer.setUser(user);
+            customer.setShippingAddress(userModel.getShippingAddress());
+            customerDetailsRepository.save(customer);
+        }
 
      return  UserModel.builder().firstName(savedUser.getFirstName()).
              lastName(savedUser.getLastName()).email(savedUser.getEmail()).password(savedUser.getPassword()).contact(savedUser.getContact()).roles(savedUser.getRoles()).registeredDate(savedUser.getRegisteredDate()).build();
