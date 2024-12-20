502a303953ce411c5dea63980fd22ac5902ed8be
ChamodiDH <chamodi3797@gmail.com>
2024-12-20 18:47:22 +0530

    Update item repository with the native query for search.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/repository/ItemServiceRepository.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/repository/ItemServiceRepository.java
@@ -2,8 +2,18 @@ package com.chamodidh.market_pulse.repository;
 
 import com.chamodidh.market_pulse.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemServiceRepository extends JpaRepository<Item,Long> {
+    @Query(value = "SELECT * FROM items " +
+            "WHERE LOWER(item_name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(brand_name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(description) LIKE LOWER(CONCAT('%', :keyword, '%'))",
+            nativeQuery = true)
+    List<Item> searchItemsByKeyword(@Param("keyword") String keyword);
 }
