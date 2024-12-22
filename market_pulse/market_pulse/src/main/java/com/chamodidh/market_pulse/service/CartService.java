06fc7f92ba013f11f774ec5210e987a4ebdc7824
ChamodiDH <chamodi3797@gmail.com>
2024-12-22 20:16:21 +0530

    Implement service for the get cart.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/CartService.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/CartService.java
@@ -2,9 +2,14 @@ package com.chamodidh.market_pulse.service;
 
 
 import com.chamodidh.market_pulse.entity.Item;
+import com.chamodidh.market_pulse.model.CartModel;
 
 import java.util.List;
 
 public interface CartService {
     List<Item> addToCart(long itemId, long userId);
+
+    String deleteItem(long itemId, long userId);
+
+    CartModel getCart(long userId);
 }
--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/CartServiceImpl.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/service/CartServiceImpl.java
@@ -2,6 +2,7 @@ package com.chamodidh.market_pulse.service;
 
 import com.chamodidh.market_pulse.entity.Cart;
 import com.chamodidh.market_pulse.entity.Item;
+import com.chamodidh.market_pulse.model.CartModel;
 import com.chamodidh.market_pulse.repository.CartRepository;
 import com.chamodidh.market_pulse.repository.ItemServiceRepository;
 import com.chamodidh.market_pulse.repository.UserRepository;
@@ -25,8 +26,28 @@ public class CartServiceImpl implements CartService{
         //save the cart to the item
         cart.getItems().add(item);
         cart.setTotalCost(cart.getTotalCost() + item.getUnitPrice());
+
         cartRepository.save(cart);
         return cartRepository.findById(cart.getId()).get().getItems();
 
     }
+
+    @Override
+    public String deleteItem(long itemId, long userId) {
+        Cart cart = userRepository.findById(userId).get().getCustomerDetails().getCart();
+        Item item = itemServiceRepository.findById(itemId).get();
+        cart.getItems().remove(item);
+        cart.setTotalCost(cart.getTotalCost() - item.getUnitPrice());
+        cartRepository.save(cart);
+        return "Removed successfully";
+
+    }
+
+    @Override
+    public CartModel getCart(long userId) {
+        Cart cart = userRepository.findById(userId).get().getCustomerDetails().getCart();
+        return CartModel.builder().id(cart.getId()).totalCost(cart.getTotalCost()).items(cart.getItems()).numberOfItems(cart.getNumberOfItems()).build();
+    }
+
+
 }
