fb48e55e448e8ca83f00cf66eabeefc2d2ffbe5d
ChamodiDH <chamodi3797@gmail.com>
2024-12-26 16:11:13 +0530

    Updated with constructors.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/model/CartModel.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/model/CartModel.java
@@ -3,15 +3,18 @@ package com.chamodidh.market_pulse.model;
 import com.chamodidh.market_pulse.entity.CustomerDetails;
 import com.chamodidh.market_pulse.entity.Item;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartModel {
 
     private Long id;
--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/model/ItemModel.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/model/ItemModel.java
@@ -1,8 +1,13 @@
 package com.chamodidh.market_pulse.model;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemModel {
 
     private float unitPrice;
