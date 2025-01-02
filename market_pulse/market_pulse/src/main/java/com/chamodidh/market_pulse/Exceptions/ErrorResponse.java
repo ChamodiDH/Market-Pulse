25f440f7a40f51f02bedfb0525f849411b29fc77
ChamodiDH <chamodi3797@gmail.com>
2025-01-02 19:12:29 +0530

    Implement centralized exception handling with GlobalExceptionHandler

---
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/Exceptions/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.chamodidh.market_pulse.Exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int statusCode;
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+}
