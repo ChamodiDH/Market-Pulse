0e1026c39e9816ccbb958ade58b9e42edabecbc7
ChamodiDH <chamodi3797@gmail.com>
2025-01-04 18:14:36 +0530

    Add password encryption.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/config/WebSecurityConfig.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/config/WebSecurityConfig.java
@@ -1,23 +1,45 @@
 package com.chamodidh.market_pulse.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
-                requests -> requests.requestMatchers("register").permitAll().
+                requests -> requests.requestMatchers("register", "login").permitAll().
                         anyRequest().authenticated()
         ).httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bycrptPasswordEncoder(){
+        return new BCryptPasswordEncoder(14);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
 }
