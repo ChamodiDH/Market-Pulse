4bfc6a140334b371326d3c541ed9e584aeb71869
ChamodiDH <chamodi3797@gmail.com>
2025-01-07 14:29:30 +0530

    Implement role based access permission.

--- market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/controller/CategoryController.java
+++ market_pulse/market_pulse/src/main/java/com/chamodidh/market_pulse/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.chamodidh.market_pulse.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping("/add")
+    @PreAuthorize("hasRole('SUPPLIER')")
     public ResponseEntity<CategoryModel> addCategory(@RequestBody CategoryModel categoryModel){
         return new ResponseEntity<>(categoryService.addCategory(categoryModel), HttpStatus.CREATED);
     }
